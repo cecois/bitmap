6f656992f250f9215bfca2ed565f79a4a5bbbb9c
= <cecmcgee@gmail.com>
2015-05-27 17:33:56 -0400

    globals back to local from lab

--- src/js/globals.js
+++ src/js/globals.js
@@ -1,4 +1,4 @@
 window.verbose = true;
 window.dev = true;
-// window.solrhost = "http://localhost:8983/solr/";
-window.solrhost = "http://solr-lbones.rhcloud.com/";
+window.solrhost = "http://localhost:8983/solr/";
+// window.solrhost = "http://solr-lbones.rhcloud.com/";
