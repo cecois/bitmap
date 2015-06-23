17edb2fe355bc68d4e3d7b4ac1e177184ef69ac6
C.C. Miller <christopher.miller@ll.mit.edu>
2015-06-23 16:59:55 -0400

    throbberjs maybe

--- src/js/views/CartoCollxCountView.js
+++ src/js/views/CartoCollxCountView.js
@@ -1,8 +1,14 @@
 var CartoCollxCountView = Backbone.View.extend({
     el: ".query-subnav-count-locations",
     initialize: function() {
+        this.listenTo(this.collection, 'request', this.throb);
         this.listenTo(this.collection, 'reset', this.render);
         return this.render()
+    },
+    throb: function(){
+
+$(".query-subnav-count-locations").html("i->")
+
     },
     render: function() {
         var len = this.collection.models.length
