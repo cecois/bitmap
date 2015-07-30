4625e3f0675a8f09b67a571c95e61bebfe918f0d
C.C. Miller <christopher.miller@ll.mit.edu>
2015-07-30 16:36:38 -0400

    oops this is the right gitignore?

--- src/lib/octo.js
+++ src/lib/octo.js
@@ -1,5 +1,6 @@
 // window.octo = new Octokat({token:"b5824e01cc47d80450bed56564d09a7a2ba7656f"});
-window.octo = new Octokat({token:"539070395ca3ceab1219717a166ec2a66edb8eac"});
+// window.octo = new Octokat({token:"539070395ca3ceab1219717a166ec2a66edb8eac"});
+window.octo = new Octokat({token:"2007762049fae89c7856fc296d994f431f74d7cd"});
 
 window.octocb = function(err,val){};
 
