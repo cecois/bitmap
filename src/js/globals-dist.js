cff4438d0a21a788e5cfaa633dacedacf73cc39a
Cecois <cecmcgee@gmail.com>
2016-10-10 19:56:27 -0400

    early Oct updatesView

--- src/js/globals-dist.js
+++ src/js/globals-dist.js
@@ -5,4 +5,4 @@ window.plierline=999;
 window.verbose = false;
 window.dev = false;
 window.solrhost = "http://solr-lbones.rhcloud.com/";
-window.queryinit = "(episode:%22http://howl.fm/audio/42848/live-from-comic-con-2016%22 OR episode: %22http://howl.fm/audio/42827/cbb-2016-tour-best-of-pt-1%22 OR episode:436)";
+window.queryinit = "(episode:%22http://howl.fm/audio/43544/september-25-the-dancehouse-manchester%22 OR episode:%22http://howl.fm/audio/43551/september-26-glee-club-nottingham%22 OR episode:%22http://howl.fm/audio/43572/september-27-hyde-park-picture-house-leeds%22 OR episode:%22http://howl.fm/audio/43608/september-28-o2-shepherd-s-bush-empire-london%22 OR episode:448)";
