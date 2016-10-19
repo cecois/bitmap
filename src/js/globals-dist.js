87795e048a475cd0ca9c0a8a6bf7e52838f302b8
Cecois <cecmcgee@gmail.com>
2016-10-19 19:17:27 -0400

    prior to an error splash branch

--- src/js/globals-dist.js
+++ src/js/globals-dist.js
@@ -5,4 +5,4 @@ window.plierline=999;
 window.verbose = false;
 window.dev = false;
 window.solrhost = "http://solr-lbones.rhcloud.com/";
-window.queryinit = "(episode:%22http://howl.fm/audio/43544/september-25-the-dancehouse-manchester%22 OR episode:%22http://howl.fm/audio/43551/september-26-glee-club-nottingham%22 OR episode:%22http://howl.fm/audio/43572/september-27-hyde-park-picture-house-leeds%22 OR episode:%22http://howl.fm/audio/43608/september-28-o2-shepherd-s-bush-empire-london%22 OR episode:448)";
+window.queryinit = "episode:449";
